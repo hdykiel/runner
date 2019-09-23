73e5b327715e23374b87b2566b4fa59348bd7e1a
Hadrien <hadrien@rstudio.com>
2019-09-23 15:19:12 +0000

    document pacer function

--- runner/R/pacer.R
+++ runner/R/pacer.R
@@ -1,10 +1,19 @@
+#' Computes target pace based on race time goal
+#'
+#' Insert description here
+#'
+#' @param target.time n integer value of race time goal (in minutes)
+#' @param race race type (e.g marathon, 5k, etc)
+#' @importFrom lubridate seconds_to_period
+#' @return target pace
+#' @export
+
 pacer <- function(target.time = 175, race = "marathon") {
   pace_min <- target.time / 26.2
   pace_sec <- pace_min*60
 
   # print target pace
   seconds_to_period(pace_sec)
-
 
 }
 
